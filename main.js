b913c3a8ac12a082f185784510bcce254cd42164
suriburi123 <83498701+suriburi123@users.noreply.github.com>
2023-04-17 16:33:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -97,7 +97,7 @@ function gotPoses(results)
 {
     if(results.length > 0)
     {
-        console.log(reuslts);
+        console.log(results);
         scoreRightWrist = results[0].pose.keypoints[10].score;
         scoreLeftWrist = results[0].pose.keypoints[9].score;
         console.log("scoreLeftWrist =" + scoreLeftWrist);
